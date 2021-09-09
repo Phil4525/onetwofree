c5bfadcd0fe8fbe1af7d03c790ac5aa6edc9ce32
philippe mariou <philippe.mariou@colombbus.org>
2021-09-09 17:07:39 +0200

    formulaire contact

---
+++ src/Controller/ContactController.php
@@ -0,0 +1,69 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\Contacts;
+use App\Form\ContactType;
+use Symfony\Component\Mime\Email;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Mailer\MailerInterface;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+
+class ContactController extends AbstractController
+{
+    /**
+     * @Route("/contact", name="contact", methods={"GET","POST"})
+     */
+    public function index(Request $request, MailerInterface $mailer): Response
+    {
+        $contact=new Contacts();
+        $form = $this->createForm(ContactType::class, $contact);
+        $form->handleRequest($request);
+        ;
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $entityManager = $this->getDoctrine()->getManager();
+            $entityManager->persist($contact);
+            $entityManager->flush();
+            
+            $user = $form->getData();
+            $url =  './uploads/' . $user->getDocument();
+
+            $mail = (new Email())
+                ->from($user->getEmail())
+                ->to('philippe.mariou@colombbus.org')
+                //->cc('cc@example.com')
+                //->bcc('bcc@example.com')
+                //->replyTo('fabien@example.com')
+                //->priority(Email::PRIORITY_HIGH)
+                ->subject('Nouvelle demande de contact')
+                ->text('Sender : '.$user->getEmail().\PHP_EOL.$user->getMessage(),'text/plain')
+                ->html('<p>See Twig integration for better HTML integration!</p>');
+                if ( $user->getDocument()) {
+                    $mail
+                        ->attachFromPath($url);
+                }
+                ;
+            $mailer->send($mail);
+
+           
+            //return $this->redirectToRoute('contact_success');
+            $url = $this->generateUrl('home');
+            return new Response("
+            <html>
+                <body>
+                    <p>Le message a bien été envoyé</p>  
+                    <a href=\"$url\">Retour</a>
+                </body>
+            </html>
+            ");
+        }
+
+        return $this->render('contact/index.html.twig', [
+            'form' => $form->createView(),
+        ]);
+    }
+
+}
---
+++ src/Form/ContactType.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Contacts;
+use Symfony\Component\Form\AbstractType;
+use Gregwar\CaptchaBundle\Type\CaptchaType;
+use Vich\UploaderBundle\Form\Type\VichFileType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class ContactType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('nom')
+            ->add('prenom')
+            ->add('entreprise')
+            ->add('email')
+            ->add('message')
+            ->add('file', VichFileType::class, [
+                'required' => false,
+                'allow_delete' => true,
+                'delete_label' => '...',
+                'download_uri' => '...',
+                'download_label' => '...',
+                'asset_helper' => true,
+            ])
+            ->add('captcha', CaptchaType::class);
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults([
+            'data_class' => Contacts::class,
+        ]);
+    }
+}
